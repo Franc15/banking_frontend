bd211eee58076b7a11b247faa313e3f9b5b9136e
Francis Kikulwe <francissteven1@gmail.com>
2023-01-07 15:33:28 +0400

    complete send login details feature

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,22 +1,57 @@
-import React from "react";
+import React, {useState} from "react";
 import './Login.css';
+import PropTypes from 'prop-types';
 
-function Login() {
+const API_URL = 'http://localhost:5000/login';
+
+
+async function loginUser(credentials) {
+    return fetch(API_URL, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(credentials)
+    })
+    .then(data => data.json())
+}
+
+function Login({ setToken}) {
+    const [username, setUserName] = useState();
+    const [password, setPassword] = useState();
+
+    const handleSubmit = async e => {
+        e.preventDefault();
+        const token = await loginUser({
+            username,
+            password
+        });
+        setToken(token);
+    }
+
+
     return (
-        <form className="login-wrapper">
+        <div className="login-wrapper">
+      <h1>Please Log In</h1>
+      <form onSubmit={handleSubmit}>
       <label>
         <p>Username</p>
-        <input type="text" />
+        <input type="text" onChange={e => setUserName(e.target.value)} />
       </label>
       <label>
         <p>Password</p>
-        <input type="password" />
+        <input type="password" onChange={e => setPassword(e.target.value)} />
       </label>
       <div>
         <button type="submit">Submit</button>
       </div>
     </form>
+    </div>
     );
 }
 
 export default Login;
+
+Login.propTypes = {
+    setToken: PropTypes.func.isRequired
+}
